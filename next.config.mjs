015fa62fe6b5f3d43edc0e7a1fcbc56c822ffebc
Ulysses-Lima-Alltech <ulysses.bitencourtlima@gmail.com>
2026-08-20 22:19:26 -0300

    Add rewrite proxy for /shomer to shomer-amber.vercel.app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/shomer', destination: 'https://shomer-amber.vercel.app/shomer' },
+      { source: '/shomer/:path*', destination: 'https://shomer-amber.vercel.app/shomer/:path*' },
+      ];
+  },
 };
 
 export default nextConfig;
